4eae8b89b466c605b1034e85e234f93be3ed6de3
milner21 <milnerene21@gmail.com>
2022-12-06 12:17:57 -0300

    login and register setting

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,19 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+import { logout, reset } from "../features/auth/authSlice";
 function Header() {
+  const navigate = useNavigate();
+  const dispatcch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatcch(logout())
+    dispatcch(reset())
+    navigate('/')
+  }
+
   return (
     <header className="header">
       <nav className="navbar navbar-expand-lg  navbar-dark bg-primary">
@@ -21,18 +34,31 @@ function Header() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+          >
             <ul className="navbar-nav">
-              <li className="nav-item active">
-                <NavLink className={"nav-link"} to={"/login"}>
-                  <FaSignInAlt /> Iniciar sesion
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className={"nav-link"} to={"/register"}>
-                  <FaUser /> Registrarse
-                </NavLink>
-              </li>
+              {user ? (
+                <li className="nav-item active">
+                  <NavLink className={"nav-link"} onClick={onLogout}>
+                    <FaSignOutAlt /> Cerrar sesion
+                  </NavLink>
+                </li>
+              ) : (
+                <>
+                  <li className="nav-item active">
+                    <NavLink className={"nav-link"} to={"/login"}>
+                      <FaSignInAlt /> Iniciar sesion
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className={"nav-link"} to={"/register"}>
+                      <FaUser /> Registrarse
+                    </NavLink>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>
